650b1874933d9ead4fdd9c60a2c4d6a439eb8b22
Victoria <victorya.ch@gmail.com>
2026-08-08 22:44:55 +0300

    Add homework 16

---
+++ HW16/task02.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arr);
+arr.splice(arr.length, 0, 11);
+console.log(arr);
+arr.splice(0, 0, 0);
+console.log(arr);
+arr.splice(0, 1);
+arr.splice(arr.length - 1, 1);
+console.log(arr);
+arr.splice(2, 1, 99);
+console.log(arr);
+console.log(arr.length);    
